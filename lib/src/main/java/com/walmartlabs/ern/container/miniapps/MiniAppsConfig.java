fc40dad1db84a0b3d21ada89a8abd8730cf42fe7
quanlt3 <v.quanlt3@vinid.net>
2020-08-13 03:18:20 +0700

    Container v1.0.0

---
+++ lib/src/main/java/com/walmartlabs/ern/container/miniapps/MiniAppsConfig.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Walmart Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.walmartlabs.ern.container.miniapps;
+
+//
+// GENERATED CODE: DO NOT MODIFY
+//
+
+import com.walmartlabs.ern.container.ElectrodeMiniAppActivity;
+
+import android.support.annotation.NonNull;
+
+import java.util.HashMap;
+
+public class MiniAppsConfig {
+    public static final HashMap<String, Class> MINIAPP_ACTIVITIES =
+            new HashMap<String, Class>() {
+                {
+                    put(MiniApps.MiniAppHelloVin.getName(), MiniApps.MiniAppHelloVin.getActivityClass());
+                }
+            };
+
+    public enum MiniApps {
+        MiniAppHelloVin("MiniAppHelloVin", MiniAppHelloVinActivity.class),
+        ;
+
+        private final String mMiniAppName;
+        private final Class<? extends ElectrodeMiniAppActivity> mActivityClass;
+
+        MiniApps(String miniAppName, Class<? extends ElectrodeMiniAppActivity> activityClass) {
+            mMiniAppName = miniAppName;
+            mActivityClass = activityClass;
+        }
+
+        @NonNull
+        public Class<? extends ElectrodeMiniAppActivity> getActivityClass() {
+            return mActivityClass;
+        }
+
+        @NonNull
+        public String getName() {
+            return mMiniAppName;
+        }
+    }
+}
